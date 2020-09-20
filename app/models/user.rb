146275b4c9c54dff81fd6b6b151717de9cd6d52d
Yuka0410 <kimi02tarou@gmail.com>
2020-09-20 12:21:55 +0900

    bundle exec rubocop -a実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,49 +3,43 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         #まだ他モデル未実装のためコメントアウト
-         #has_many :items
-         #has_many :purchases
+  # まだ他モデル未実装のためコメントアウト
+  # has_many :items
+  # has_many :purchases
 
+  # 注意！見本はfirst_name,last_nameにしているが本アプリはfamily_nameとfirst_nameにしてしまった
 
-  #注意！見本はfirst_name,last_nameにしているが本アプリはfamily_nameとfirst_nameにしてしまった
-  
-
-  with_options presence: true do 
-         validates :nickname
-         validates :birthday
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
   end
-         validates :password, presence: true
-         validates :password, length: { minimum: 6 } #,{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :password_confirmation, presence: true             
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #PASSWORD_REGEXの変数にpasswordを代入している
-          validates_format_of :password, with: PASSWORD_REGEX, message: "include both letters and numbers" 
-          validates_format_of :password_confirmation, with: PASSWORD_REGEX, message: "include both letters and numbers"
-       
-  
-  
-  #with_options presence: true do もしdryに沿って文章整理するならこれ使う/error文変える必要あるため注意。
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 } # ,{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze # PASSWORD_REGEXの変数にpasswordを代入している
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers'
+  validates_format_of :password_confirmation, with: PASSWORD_REGEX, message: 'include both letters and numbers'
 
-    validates :family_name, presence: true 
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
-    validates :first_name, presence: true
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
-  #end
+  # with_options presence: true do もしdryに沿って文章整理するならこれ使う/error文変える必要あるため注意。
 
-  #with_options presence: true do
-    validates :family_name_kana, presence: true
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-    validates :first_name_kana, presence: true
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-  #end
+  validates :family_name, presence: true
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+  validates :first_name, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+  # end
+
+  # with_options presence: true do
+  validates :family_name_kana, presence: true
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :first_name_kana, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  # end
 
   validates :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }#これは大文字、小文字を区別しなくなる
-                    #メッセージはブラウザに出る？
+  validates :email, uniqueness: { case_sensitive: false } # これは大文字、小文字を区別しなくなる
+  # メッセージはブラウザに出る？
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  
-
 end
 
-#validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
- #もし後でメッセージ表記も含めるなら上記のように書く。参照：寄付アプリのところ。
+# validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+# もし後でメッセージ表記も含めるなら上記のように書く。参照：寄付アプリのところ。
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,99 +5,98 @@ RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.build(:user)
     end
-    
+
     it 'すべての値が正しく入力されていれば保存できること' do
-     expect(@user).to be_valid
+      expect(@user).to be_valid
     end
     it 'nicknameが空だと保存できないこと' do
-      @user.nickname = ""
+      @user.nickname = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
-    it 'family_nameが空だと保存できないこと' do  #バリデーションに重複あり、エラーメッセージ２つ書く
-      @user.family_name = ""
+    it 'family_nameが空だと保存できないこと' do  # バリデーションに重複あり、エラーメッセージ２つ書く
+      @user.family_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Family name can't be blank", "Family name Full-width characters")
+      expect(@user.errors.full_messages).to include("Family name can't be blank", 'Family name Full-width characters')
     end
     it 'family_nameが全角日本語でないと保存できないこと' do
-      @user.family_name = "ﾔﾏﾀﾞ"
+      @user.family_name = 'ﾔﾏﾀﾞ'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Family name Full-width characters")
+      expect(@user.errors.full_messages).to include('Family name Full-width characters')
     end
     it 'first_nameが空だと保存できないこと' do
-      @user.first_name = ""
+      @user.first_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("First name can't be blank")
     end
     it 'first_nameが全角日本語でないと保存できないこと' do
-     @user.first_name = "ﾘｸﾀﾛｳ"
-     @user.valid?
-     expect(@user.errors.full_messages).to include("First name Full-width characters")
+      @user.first_name = 'ﾘｸﾀﾛｳ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name Full-width characters')
     end
     it 'family_name_kanaが全角カタカナでないと保存できないこと' do
-      @user.family_name_kana = "ﾔﾏﾀﾞ"
+      @user.family_name_kana = 'ﾔﾏﾀﾞ'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Family name kana Full-width katakana characters")
+      expect(@user.errors.full_messages).to include('Family name kana Full-width katakana characters')
     end
     it 'first_name_kanaが全角カタカナでないと保存できないこと' do
-      @user.first_name_kana = "ﾘｸﾀﾛｳ"
+      @user.first_name_kana = 'ﾘｸﾀﾛｳ'
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name kana Full-width katakana characters")
+      expect(@user.errors.full_messages).to include('First name kana Full-width katakana characters')
     end
     it 'email空だと保存できないこと' do
       @user.email = nil
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
-    it 'emailが@を含んでいないと保存できないこと' do          
-      @user.email = "aaa.aaa.com"
+    it 'emailが@を含んでいないと保存できないこと' do
+      @user.email = 'aaa.aaa.com'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
-    it "重複したemailが存在する場合登録できない" do
+    it '重複したemailが存在する場合登録できない' do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
-    it 'passwordは空だと保存できないこと' do                 
-      @user.password = ""
+    it 'passwordは空だと保存できないこと' do
+      @user.password = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordが5文字以下では保存できないこと' do
-      @user.password = "00000"
-      @user.password_confirmation = "00000"
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
-    it 'passwordは半角英数を含まないと保存できないこと' do             
-      @user.password = "aaaaaa"
-      @user.password_confirmation = "aaaaaa"
+    it 'passwordは半角英数を含まないと保存できないこと' do
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = 'aaaaaa'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password include both letters and numbers", "Password confirmation include both letters and numbers")
+      expect(@user.errors.full_messages).to include('Password include both letters and numbers', 'Password confirmation include both letters and numbers')
     end
     it 'password_confirmationが空だと保存できないこと' do
       @user.password = nil
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
-    it "passwordが存在してもpassword_confirmationが空では保存できない" do
-       @user.password_confirmation = ""
-       @user.valid?
-       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    it 'passwordが存在してもpassword_confirmationが空では保存できない' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'birthdayが空では保存できないこと' do
-       @user.birthday = nil
-       @user.valid?
-       expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      @user.birthday = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
 end
 
+# it 'password_confirmationがpasswordと一致しないと保存できないこと' do
+# devise が表示してくれるので不要！
 
-# it 'password_confirmationがpasswordと一致しないと保存できないこと' do  
-#devise が表示してくれるので不要！
-
-#passwordの一意性はセキリュティ面でできないので不要
+# passwordの一意性はセキリュティ面でできないので不要

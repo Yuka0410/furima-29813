5f1212d9fcfbd78b238110631169c36462ea4810
Yuka0410 <kimi02tarou@gmail.com>
2020-09-20 12:16:25 +0900

    ユーザー管理機能単体テストコード導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,36 +6,43 @@ class User < ApplicationRecord
          #まだ他モデル未実装のためコメントアウト
          #has_many :items
          #has_many :purchases
-#ここのend必要？
+
 
   #注意！見本はfirst_name,last_nameにしているが本アプリはfamily_nameとfirst_nameにしてしまった
-  validates :password_confirmation, presence: {message: "doesn't match Password"}
+  
 
   with_options presence: true do 
          validates :nickname
          validates :birthday
+  end
+         validates :password, presence: true
          validates :password, length: { minimum: 6 } #,{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-                      
+         validates :password_confirmation, presence: true             
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #PASSWORD_REGEXの変数にpasswordを代入している
-          validates_format_of :password, with: PASSWORD_REGEX, message: "Password Include both letters and numbers" 
-          validates_format_of :password_confirmation, with: PASSWORD_REGEX, message: "Password_confirmation Include both letters and numbers"
-  end     
+          validates_format_of :password, with: PASSWORD_REGEX, message: "include both letters and numbers" 
+          validates_format_of :password_confirmation, with: PASSWORD_REGEX, message: "include both letters and numbers"
+       
   
   
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"} do
-    validates :family_name
-    validates :first_name
-  end
+  #with_options presence: true do もしdryに沿って文章整理するならこれ使う/error文変える必要あるため注意。
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"} do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
+    validates :family_name, presence: true 
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
+    validates :first_name, presence: true
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
+  #end
+
+  #with_options presence: true do
+    validates :family_name_kana, presence: true
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :first_name_kana, presence: true
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  #end
 
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }#これは大文字、小文字を区別しなくなる
                     #メッセージはブラウザに出る？
-
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+        nickname              {"ABC子"}                     #{Faker::Name.initials}
+        email                 {"aaa@aaa.com"}                       #{Faker::Internet.free_email}
+        password              {"abc123"}
+        password_confirmation {password}
+        family_name { '山田' }
+        first_name { '隆太郎' }
+        family_name_kana { 'ヤマダ' }
+        first_name_kana { 'リクタロウ' }
+        birthday { Date.new(2000, 11, 12) } 
+  end
+    
+end

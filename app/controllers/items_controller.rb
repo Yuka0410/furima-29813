e3bdc20bc1d3ce077cb0d329c7d7fe5a9ca67ecd
Yuka0410 <kimi02tarou@gmail.com>
2020-09-21 14:13:20 +0900

    ビュー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,19 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all #itemの機能全て見える（あとで変更するかも
+    @items = Item.all
   end
 
-#以下まだ実装必要なし
+  def new
+    @items = Item.new  #商品新規入力GET
+  end
 
-  #def new
-   # @items = Item.new  #商品新規入力GET
-  #end
-
-  #def create   #商品保存POST
-   # if @items = Item.save #saveでif文のように保存成功可否を判定
-    # redirect_to root_path
-    #else
-    #  render :new
-    #end
-  #end
+  def create   #商品保存POST
+    if @items = Item.save #saveでif文のように保存成功可否を判定
+      redirect_to root_path #itemの情報を持っていかない
+    else
+      render :new #itemの情報を持っていく
+    end
+  end
 
   #def show
    # @items = Item.show
@@ -24,4 +22,8 @@ end
 
 
 
- private
+private
+
+def item_params           #imageはActiveStorage使用
+  params.require(:item).permit(:name, :text, :category, :condition, :payer, :area, :delivery_date, :price).merge(user_id: current_user.id)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'  
-  resources :items do   #あとでpurchaseのresourcesネストするか？（スクショ参照）
-  end
+  resources :items, only: [:index, :new, :create] #あとでpurchaseのresourcesネストするか？（スクショ参照）
+  #end #resources :items doにするならいれる
 end

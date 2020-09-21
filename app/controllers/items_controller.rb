a17abe6497a00a517194ff825f31c776b92f9b99
Yuka0410 <kimi02tarou@gmail.com>
2020-09-21 09:07:21 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all #itemの機能全て見える（あとで変更するかも
   end
+
+#以下まだ実装必要なし
+
+  #def new
+   # @items = Item.new  #商品新規入力GET
+  #end
+
+  #def create   #商品保存POST
+   # if @items = Item.save #saveでif文のように保存成功可否を判定
+    # redirect_to root_path
+    #else
+    #  render :new
+    #end
+  #end
+
+  #def show
+   # @items = Item.show
+  #end
 end
+
+
+
+ private

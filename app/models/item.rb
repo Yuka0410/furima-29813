9e530815bae0c7bfa7e222672c9b23c41eddde08
Yuka0410 <kimi02tarou@gmail.com>
2020-09-22 11:40:42 +0900

    ActiveHush実装完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,20 @@ class Item < ApplicationRecord
    #has_one :purchase
    extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to_active_hash :category
+   belongs_to_active_hash :condition
+   belongs_to_active_hash :payer
+   belongs_to_active_hash :area
+   belongs_to_active_hash :delivery_date
 
-                            #↓のちに他のカラム付け足す？
+                        
    validates :name, :text, :category, presence: true
    validates :category_id, numericality: { other_than: 1 }
+   validates :name, :text, :condition, presence: true
+   validates :conditon_id, numericality: { other_than: 1 }
+   validates :name, :text, :payer, presence: true
+   validates :payer_id, numericality: { other_than: 1 }
+   validates :name, :text, :area, presence: true
+   validates :area_id, numericality: { other_than: 0 }
+   validates :name, :text, :delivery_date, presence: true
+   validates :delivery_date_id, numericality: { other_than: 1 }
 end

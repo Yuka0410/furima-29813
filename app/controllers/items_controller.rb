523ed043cee3a0eb34ae05018059c28fcce17ec8
Yuka0410 <kimi02tarou@gmail.com>
2020-09-28 16:13:08 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,29 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
-    @item = Item.new  
+    @item = Item.new
   end
 
-  def create 
-    @item = Item.new(item_params) 
+  def create
+    @item = Item.new(item_params)
     if @item.valid?
       @item.save
-      redirect_to root_path 
+      redirect_to root_path
     else
-      render :new 
+      render :new
     end
   end
 
   def show
-   @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def edit
-   @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
@@ -32,14 +32,13 @@ class ItemsController < ApplicationController
     if @item.valid?
       redirect_to item_path
     else
-     render :edit
+      render :edit
     end
   end
 
-
   private
 
-  def item_params          
+  def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :payer_id, :area_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end

8ae2de0e666db936360799615b2b2e6a05535d90
Yuka0410 <kimi02tarou@gmail.com>
2020-09-29 15:58:09 +0900

    purchaseルーティング修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,14 @@
 class PurchasesController < ApplicationController
   
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
   
 end
+
+private
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'  
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]  
-  resources :purchases, only: [:create]#多分違うと思うので後で修正
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :purchases, only: [:index, :create]
+  end
 end

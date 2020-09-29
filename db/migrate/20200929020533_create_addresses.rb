b84a08b70b814b62e9e8f922ffab11d7103cc46a
Yuka0410 <kimi02tarou@gmail.com>
2020-09-29 11:46:28 +0900

    マイグレーションファイル作成

--- db/migrate/20200929020533_create_addresses.rb
+++ db/migrate/20200929020533_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code,    null: false, default: ""  
+      t.integer :prefecture,    null: false
+      t.string :city,           null: false, default: ""
+      t.string :block,          null: false, default: ""
+      t.string :building_name,               default: ""
+      t.string :phone_number,   null: false, default: ""
+      t.references :purchase,   null: false,  foreign_key: true
       t.timestamps
     end
   end

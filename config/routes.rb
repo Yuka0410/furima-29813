c3fddff1d9e8f9b7f2e75375c06e3d51a0efeb11
Yuka0410 <kimi02tarou@gmail.com>
2020-09-29 14:15:27 +0900

    viewのディレクトリをordersからpurchasesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'  
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]  
-  resources :purchases, only: [:create]
+  resources :purchases, only: [:create]#多分違うと思うので後で修正
 end

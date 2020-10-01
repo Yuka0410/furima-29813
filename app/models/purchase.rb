f9b2935732c4c8183eb2a06af3f3be2fc96ed5a5
Yuka0410 <kimi02tarou@gmail.com>
2020-10-01 10:45:46 +0900

    アソシエーション追加

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,12 @@
 class Purchase < ApplicationRecord
+  attr_accessor :token #Payjpに値段情報送る必要があるなら , :price を入れて
+
   belongs_to :user
   belongs_to :item
   has_one :address
+
+  validates :token, presence: true #他のバリデーションは他のモデルにいれるか
+  
+
+
 end

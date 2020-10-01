aac053620745dccca4e22bc0ae4ad3eeda6c489e
Yuka0410 <kimi02tarou@gmail.com>
2020-10-01 17:41:35 +0900

    エラーメッセージ実装

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -5,27 +5,23 @@ class PurchaseAddress
   attr_accessor :token 
 
     with_options presence: true do #購入ページでは値段は表示だけでありvalidation不要。area_idはaddressモデルに別途保存するので必要。
-        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
         validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/ , message: "is invalid. Input Include half-width numbers"} 
+        validates :phone_number,  length: { maximum: 11 } 
         validates :area_id, numericality: { other_than: 0, message: 'Select' }
         validates :token
         validates :city
         validates :block   
     end
        
-        #building_nameは任意のため正規表現だけつける
-       NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-        validates_format_of :city, with: NAME_REGEX, message: 'Full-width characters'
-        validates_format_of :block, with: NAME_REGEX, message: 'Full-width characters'
-        validates_format_of :building_name, with: NAME_REGEX, message: 'Full-width characters'
-
+       
    def save
      purchase = Purchase.create(user_id: user_id, item_id: item_id)
      #purchaseという変数にpurchase.createで保存
   
      Address.create(postal_code: postal_code, area_id: area_id, city: city, block: block, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
      #purchase保存してからaddressを保存する順番でないとaddressは保存できない
-    end
+   end
     
 
 end

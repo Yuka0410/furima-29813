f4df1b304a435fb820ab2ec34c7c0625c3154af6
Yuka0410 <kimi02tarou@gmail.com>
2020-10-02 15:16:47 +0900

    不要なファイル消去

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,28 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  before do
-    @order = FactoryBot.build(:order)
+  describe '商品購入しトークン、住所保存' do
+    before do
+      @purchase_address = FactoryBot.build(:purchase_address)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      #expect(@order).to be_valid
+    end
+    it 'tokenが空では登録できないこと' do
+     # @order.token = nil
+     #@order.valid?
+     #expect(@order.errors.full_messages).to include("Token can't be blank")
+    end
+    it 'postal_codeが空だと保存できないこと' do
+    end
+    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+    end
+    it 'area_idを洗濯していないと保存できないこと' do
+    end
+    it 'cityが空だと保存できないこと' do
+    end
+    it 'blockが空だと保存できないこと' do
+    end
+    it 'building_nameは空でも保存できること' do
+    end
+    it 'phone_numberが空だと保存できないこと' do
+    end
+    it 'phone_numberが半角数字のハイフンを含まない形式でないと保存できないこと' do
+    end
   end
-#後で記入内容変更するので今は仮置きしておくこと。
-  it "priceとtokenがあれば保存ができること" do
-    expect(@order).to be_valid
-  end
-
-  it "priceが空では登録できないこと" do
-    @order.price = nil
-    @order.valid?
-    expect(@order.errors.full_messages).to include("Price can't be blank")
-  end
-
-  it "tokenが空では登録できないこと" do
-    @order.token = nil
-    @order.valid?
-    expect(@order.errors.full_messages).to include("Token can't be blank")
-  end
-end
-
-
-end
-
 end
